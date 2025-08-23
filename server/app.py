4e99d097951fa14ab51c88bfc70abe97446ccbde
Michael Munga <micknyalle@gmail.com>
2025-08-23 13:52:17 +0300

    add Role model and establish one-to-many relationship with User

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 from dotenv import load_dotenv
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
--- server/models.py
+++ server/models.py
@@ -11,7 +11,7 @@ db = SQLAlchemy(metadata=MetaData(naming_convention={
     "pk": "pk_%(table_name)s"
 }))
 
-# user mode
+# user model
 # ----------------------
 
 class User(db.Model,SerializerMixin):
@@ -22,9 +22,28 @@ class User(db.Model,SerializerMixin):
     last_name = db.Column(db.String(50), nullable = False)
     email = db.Column(db.String(100) ,unique=True ,nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(20), nullable=False)
+    
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+
+    role = db.relationship("Role", back_populates="users")
+
+    serialize_rules = ('-password_hash', '-role.users',)
 
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+  
+    
+
+# role model
+# ----------------------
+class Role(db.Model, SerializerMixin):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    role_name = db.Column(db.String(50), unique=True, nullable=False)
+
+    # one to many---> one role many users
+    users = db.relationship("User", back_populates="role")
+
     
