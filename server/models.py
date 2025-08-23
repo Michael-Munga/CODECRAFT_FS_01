b9e6ce63459d3bf3d2aee1dc6f73abc3ccb5ec38
Michael Munga <micknyalle@gmail.com>
2025-08-23 12:15:46 +0300

    set up SQLAlchemy with naming conventions and add initial User model

---
+++ server/models.py
@@ -0,0 +1,30 @@
+from flask_sqlachemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+# naming convention
+db = SQLAlchemy(metadata=MetaData(naming_convention={
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}))
+
+# user mode
+# ----------------------
+
+class User(db.Model,SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.integer,primary_key= True)
+    first_name = db.Column(db.string(50), nullable = False)
+    last_name = db.Column(db.string(50), nullable = False)
+    email = db.Column(db.string(100) ,unique=True ,nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
